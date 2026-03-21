e7f91169d44e53cdfa7c8497fa42c4e42b5f56a5
rakibwdev <rakib.wdev@gmail.com>
2026-03-21 22:37:05 +0600

    made front page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,29 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'User 1',
+            'email' => 'user1@example.com',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User 2',
+            'email' => 'user2@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'User 3',
+            'email' => 'user3@example.com',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'users' => User::all()
+    ]);
 });
