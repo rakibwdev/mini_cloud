882688020797ee8dce6b18dda7b307c6d65b5526
rakibwdev <rakib.wdev@gmail.com>
2026-03-23 20:49:34 +0600

    auth route send to web file

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
-// Auth Routes
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Storage Routes (Protected by Auth)
 Route::middleware('auth:sanctum')->group(function () {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
